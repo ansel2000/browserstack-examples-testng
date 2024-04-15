6561e42ff82dfad181e77cb3ebe2b0952fe1cb28
sanket <sanket.ma@browserstack.com>
2024-04-15 13:06:56 +0530

    update loginTest.java

--- src/test/java/com/browserstack/test/suites/login/LoginTest.java
+++ src/test/java/com/browserstack/test/suites/login/LoginTest.java
@@ -20,28 +20,25 @@ public class LoginTest extends TestBase {
         driver.findElement(By.cssSelector("#password input")).sendKeys("testingisfun99" + Keys.ENTER);
         driver.findElement(By.id("login-btn")).click();
         Assert.assertEquals(driver.findElement(By.className("api-error")).getText(), "Your account has been locked.");
-        PercySDK.snapshot(driver,"Locked User");
+        PercySDK.snapshot(driver, "Locked User");
     }
 
     @Test
     public void loginSuccess() {
-        PercySDK.snapshot(driver,"Home Page");
+        PercySDK.snapshot(driver, "Home Page");
         driver.findElement(By.id("signin")).click();
         driver.findElement(By.cssSelector("#username input")).sendKeys("fav_user" + Keys.ENTER);
         driver.findElement(By.cssSelector("#password input")).sendKeys("testingisfun99" + Keys.ENTER);
-        HashMap<String, Object> options1 = new HashMap<String, Object>();
         HashMap<String, Object> options = new HashMap<String, Object>();
         String randomColorBtn = "#" + String.format("%06x", new Random().nextInt(0xffffff + 1));
-        options1.put("percyCSS", ".login_wrapper .Modal_modal__3I0HK .Button_root__24MxS{background-color: "+randomColorBtn+";}");
-        PercySDK.snapshot(driver,"Login Page");
+        options.put("percyCSS", ".login_wrapper .Modal_modal__3I0HK .Button_root__24MxS{background-color: " + randomColorBtn + ";}");
+        PercySDK.snapshot(driver, "Login Page");
         driver.findElement(By.id("login-btn")).click();
         Assert.assertEquals(driver.findElement(By.className("username")).getText(), "fav_user");
         HashMap<String, Object> options1 = new HashMap<String, Object>();
         String randomColor = "#" + String.format("%06x", new Random().nextInt(0xffffff + 1));
-
-        options.put("percyCSS", " .shelf-container .shelf-item{background-color: "+randomColor+";}");
-
-        PercySDK.snapshot(driver,"LoggedIn Page",options1);
+        options1.put("percyCSS", " .shelf-container .shelf-item{background-color: " + randomColor + ";}");
+        PercySDK.snapshot(driver, "LoggedIn Page", options1);
     }
 
     @Test
@@ -51,7 +48,7 @@ public class LoginTest extends TestBase {
         driver.findElement(By.cssSelector("#password input")).sendKeys("wrongpass" + Keys.ENTER);
         driver.findElement(By.id("login-btn")).click();
         Assert.assertEquals(driver.findElement(By.className("api-error")).getText(), "Invalid Password");
-        PercySDK.snapshot(driver,"Invalid Login");
+        PercySDK.snapshot(driver, "Invalid Login");
     }
 
 }
